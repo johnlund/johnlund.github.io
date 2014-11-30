a0c148d0a5d4274c46cc93ce8c7358218ce57a61
John Lund <john.lund@waysact.com>
2014-11-30 13:54:59 -0800

    importing lunddogg.com

---
+++ js/functions.js
@@ -0,0 +1,69 @@
+var numPages = 14;
+var inverted = false;
+
+if (document.images)
+{
+	preload_image_object = new Image();
+	// set image url
+	image_url = new Array();
+	image_url[0] = "images/logo_inv.png";
+	image_url[1] = "images/logo.png";
+	
+	var i = 0;
+	for(i=0; i<=1; i++) 
+		preload_image_object.src = image_url[i];
+}
+
+$.ajaxSetup({
+	cache: false
+});
+
+$(document).ready(function() {
+	$('#logo').mouseenter(function(e){
+		$(e.target).css('background-image','url("images/logo_inv.png")');
+		$('body').css('background-color','#000000');
+	});
+	$('#logo').mouseleave(function(e){
+		$(e.target).css('background','none');
+		$('body').css('background-color','#FFFFFF');
+		$('#container').load('home.html', function() {
+			createIdeas();
+			if (window.location.hash) {
+				var hash = window.location.hash.replace(/^.*#/, '');
+				$('#content').load('content'+hash+'.html');
+			}
+		});
+	});
+});
+
+
+function createIdeas() {
+	for (i=1;i<=numPages;i++) {
+		$('<div id="'+i+'" class="idea"></div>').appendTo('#ideas');
+	}
+
+	if (!inverted) {
+		$('.idea').mouseenter(function(e) {
+			$('.idea').css('background-color', '#FFFFFF');
+			$('body').css('background-color', '#000000');
+		});
+		$('.idea').mouseleave(function(e) {
+			$('.idea').css('background-color', '#000000');
+			$('body').css('background-color', '#FFFFFF');
+		});
+	}
+	else {
+		$('.idea').mouseenter(function(e) {
+			$('.idea').css('background-color', '#000000');
+			$('body').css('background-color', '#FFFFFF');
+		});
+		$('.idea').mouseleave(function(e) {
+			$('.idea').css('background-color', '#FFFFFF');
+			$('body').css('background-color', '#000000');
+		});
+	}
+
+	$('.idea').click(function(e) {
+		$('#content').load('content/'+e.target.id+'.html');
+	});
+}
